6314df97d146a021d30cd72ac2a7eb2698f04b9f
chendytriwardani <chendytriwardani2@gmail.com>
2022-03-13 00:49:45 +0700

    Tugas no.2

---
+++ variable.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal Nomor 2</title>
+</head>
+<body>
+    <h1>Nama : <?php echo "Chendy Tri Wardani";?><h1> 
+    <h1>Nim : <?php echo "200411100014";?><h1>
+    <?php
+    $num1 = 8; 
+    $num2 = 7; 
+    $num3 = 6; 
+    // operasi matematika
+    $hasil1 = $num1 + $num2;
+    $hasil2 = $num2 + $num1;
+    $hasil3 = $num2 + $num3;
+
+    echo" 1. penjumlahan operasi matemarika $num1 + $num2 = $hasil1 <br>"; 
+    echo" 2. pengurangan operasi matemarika $num2 - $num1 = $hasil2 <br>"; 
+    echo" 3. perkalian operasi matemarika $num2 * $num3 = $hasil3 <br>"; 
+    
+    ?>
+</body>
+</html>
