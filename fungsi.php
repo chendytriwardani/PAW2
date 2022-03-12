8275873a5594a5754d2b4dad5a522e1e89760bdd
chendytriwardani <chendytriwardani2@gmail.com>
2022-03-13 00:50:19 +0700

    Tugas no.3

---
+++ fungsi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal nomor 3</title>
+</head>
+<body>
+    <h3><?php echo "PENAMBAHAN MENGGUNAKAN FUNGSI"; ?></h3>
+    <?php
+    // membuat fungsi php
+    
+    function penjumlahan($a, $b){
+        echo "$a + $b = ", $a+$b ,"<br>";
+    }
+
+    penjumlahan(5,6); 
+    penjumlahan(7,6); 
+    penjumlahan(8,6); 
+    ?>
+
+    
+</body>
+</html>
