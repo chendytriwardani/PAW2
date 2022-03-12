ef303014b494d58671cab435211147600e7b9c77
chendytriwardani <chendytriwardani2@gmail.com>
2022-03-13 00:40:50 +0700

    tugas no.1

---
+++ hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal Nomor 1</title>
+</head>
+<body>
+    <h1>Nama : <?php echo "Chenndy Tri Wardani";?><h1> 
+    <h1>Nim : <?php echo "200411100041";?><h1>
+</body>
+</html>
